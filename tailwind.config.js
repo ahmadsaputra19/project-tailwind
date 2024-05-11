32ca560b1dcbe7eba87a49499723e33644341f6b
ahmadsaputra19 <rebahankan22@gmail.com>
2024-05-11 16:42:51 +0700

    menambahkan style pada index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors :{
+        primary: '#0d9488',
+        dark: '#0f172a'
+      }
+    },
   },
   plugins: [],
 }
